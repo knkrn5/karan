4572b7a7bd0db7a9a81e1c6582bf3fdc9c5320ac
Karan <xkaranx5@gmail.com>
2025-02-07 21:37:19 +0530

    ordered files

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import Header from "./components/common/header";
-import Footer from "./components/common/footer";
-import { Outlet } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 import { ThemeProvider } from "./contexts/themeProvider";
 
+import Header from "./components/common/header";
+import Footer from "./components/common/footer";
+import Home from "./components/home/home";
+import ErrorPage from "./pages/errors/errorPage";
+import AboutMe from "./pages/about";
+import ResourcesInfo from "./pages/resources/resoursesPage";
 
 function App() {
   return (
-    <ThemeProvider>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider>
+        <Header />
+        <main>
+          <Routes>
+              <Route index element={<Home />} />
+              <Route path="*" element={<ErrorPage />} />
+
+              <Route path="/resources" element={<ResourcesInfo />} />
+
+              <Route path="/about" element={<AboutMe />} />
+          </Routes>
+        </main>
+        <Footer />
+      </ThemeProvider>
+    </Router>
   );
 }
 
