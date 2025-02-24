407ee0627c5b52292401fdbaaef2c0fde36112fb
Karan <xkaranx5@gmail.com>
2025-02-24 23:01:16 +0530

    axios added

--- frontend/src/contexts/themes/ThemeContext.tsx
+++ frontend/src/contexts/themes/ThemeContext.tsx
@@ -8,11 +8,3 @@ type ThemeContextType = {
 export const ThemeContext = createContext<ThemeContextType>(
   {} as ThemeContextType
 );
-
-/* export const useThemeContext = () => {
-  const context = useContext(ThemeContext);
-  if (context === null) {
-    throw new Error("useThemeContext must be used within a ThemeProvider");
-  }
-  return context;
-}; */
