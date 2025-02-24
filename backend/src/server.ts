91f0be1690408b968bbda804f133de27eb16fcc1
Karan <xkaranx5@gmail.com>
2025-02-24 22:07:18 +0530

    prod

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -26,7 +26,7 @@ app.get("/health", (req: Request, res: Response) => {
   res.send("health ok :)");
 })
 
-/* app.get('*', (req, res) => {
-  res.redirect(process.env.FRONTEND_URL);
-  // res.redirect('http://localhost:5173');
-}); */
+app.get('*', (req, res) => {
+  // res.redirect(process.env.FRONTEND_URL);
+  res.redirect('http://localhost:5173');
+});
