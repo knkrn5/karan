da69743d2a7f5a0983632b98f8b1e1dbdc4c6be2
Karan <xkaranx5@gmail.com>
2025-03-05 15:01:43 +0530

    loadin in logout

--- frontend/src/pages/auth/logout.tsx
+++ frontend/src/pages/auth/logout.tsx
@@ -1,32 +1,37 @@
 import { IoLogOut } from 'react-icons/io5';
 import axios from 'axios';
 import { useProfileStore } from '../../stores/auth/authUserProfileStore';
+import { useState } from 'react';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 // import { useNavigate } from 'react-router';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 export default function Logout() {
   // const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleLogout = async () => {
     try {
+      setIsLoading(true);
       const response = await axios.post(`${API_URL}/api/v1/auth/user/logout`, { withCredentials: true });
       console.log(response);
       useProfileStore.getState().setIsSuccessLoginedIn(false);
       localStorage.removeItem('isSuccessLoginedInLs');
+      console.log('calling logout');
     } catch (error) {
       console.log(error);
     } finally {
       // navigate('/');
       // window.location.reload();
+      setIsLoading(false);
     }
   };
-
 
   return (
     <>
       <button type="button" title="Logout" className="bg-blue-500 dark:bg-blue-600 text-white p-2 rounded-md cursor-pointer duration-300 hover:bg-blue-800" onClick={handleLogout}>
-        <IoLogOut className="w-5 h-5 duration-300 hover:scale-125" />
+        {isLoading ? <AiOutlineLoading3Quarters className="animate-spin h-5 w-5 " /> : <IoLogOut className="w-5 h-5 duration-300 hover:scale-125" />}
       </button>
     </>
   );
