0bd227ddd576ccb76067f16292a44e02e6a97ee2
Karan <xkaranx5@gmail.com>
2025-03-08 08:45:58 +0530

    prettier

--- backend/src/controllers/contact.controller.ts
+++ backend/src/controllers/contact.controller.ts
@@ -2,12 +2,12 @@ import { Request, Response } from 'express';
 import { Contact } from '../models/contact.model.js';
 import { apiResponse } from '../utils/apiResponse.js';
 
-// Adding Contact information
+// Adding Contact information in db
 const contactInfo = async (req: Request, res: Response): Promise<void> => {
   try {
     const { name = '', email = '', message = '' } = req.body;
 
-    const isFieldEmpty: boolean = [name, email, message].some((field) => field.trim().length === 0);
+    const isFieldEmpty: boolean = [name, email, message].some(field => field.trim().length === 0);
 
     if (isFieldEmpty) {
       res.status(400).json(new apiResponse(false, 'All fields are required', req.body));
@@ -34,12 +34,22 @@ const updateContactMessage = async (req: Request, res: Response): Promise<void> 
   try {
     const { id, message } = req.body;
 
+    // prettier-ignore
     if (!id) {
-      res.status(400).json( new apiResponse(false, 'Invalid ID.', null));
+      res.status(400).json(
+        new apiResponse(
+          false, 
+          'Invalid ID.',
+          null
+        ));
       return;
     }
 
-    const updatedContact = await Contact.findByIdAndUpdate(id, { $set: { message: message, status: 'updated' } }, { new: true });
+    const updatedContact = await Contact.findByIdAndUpdate(
+      id,
+      { $set: { message: message, status: 'updated' } },
+      { new: true }
+    );
 
     if (!updatedContact) {
       res.status(404).json(new apiResponse(false, 'Message not found.', null));
