cd16b534839dd571aac15e1009b40807f9592772
Karan <xkaranx5@gmail.com>
2025-03-10 09:50:53 +0530

    refactor: update contact middleware to use class structure for rate limiting

--- backend/src/middlewares/contact.middleware.ts
+++ backend/src/middlewares/contact.middleware.ts
@@ -1,20 +1,20 @@
 import rateLimit from 'express-rate-limit';
 import { ApiResponse } from '../utils/apiResponse.js';
 
-const contactPostLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000,
-  max: 3,
-  standardHeaders: true,
-  legacyHeaders: false,
-  message: new ApiResponse(false, 'Too many requests sent...', null),
-});
+export class conactMiddleware {
+  static ContactPostLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 3,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: new ApiResponse(false, 'Too many requests sent...', null),
+  });
 
-const contactPutLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000,
-  max: 2,
-  standardHeaders: true,
-  legacyHeaders: false,
-  message: new ApiResponse(false, 'Too many requests sent...', null),
-});
-
-export { contactPostLimiter, contactPutLimiter };
+  static ContactPutLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 2,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: new ApiResponse(false, 'Too many requests sent...', null),
+  });
+}
--- backend/src/routes/contact.routes.ts
+++ backend/src/routes/contact.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { contactPostLimiter, contactPutLimiter } from "../middlewares/contact.middleware.js";
+import { conactMiddleware} from "../middlewares/contact.middleware.js";
 import { ContactController } from "../controllers/contact.controller.js";
 
 const router = express.Router();
 
-router.post("/message", contactPostLimiter, ContactController.addContactMessage);
+const contactPutLimiter = conactMiddleware.ContactPutLimiter;
+router.post("/message", conactMiddleware.ContactPostLimiter, ContactController.addContactMessage);
 router.put("/message", contactPutLimiter, ContactController.updateContactMessages);
 router.delete("/message", ContactController.deleteContactMessages);
 
