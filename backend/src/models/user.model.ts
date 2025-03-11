7bef24a55b5283a48da369bee588e4cb292daa79
Karan <xkaranx5@gmail.com>
2025-03-11 15:16:34 +0530

    token

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -41,7 +41,7 @@ userSchema.methods.createAccessToken = function (): string {
   return jwt.sign(
     { userId: this._id, email: this.email },
     process.env.ACCESS_TOKEN_SECRET as string,
-    { expiresIn: '1m' }
+    { expiresIn: '15m' }
   );
 };
 
@@ -49,7 +49,7 @@ userSchema.methods.createRefreshToken = function (): string {
   return jwt.sign(
     { userId: this._id },
     process.env.REFRESH_TOKEN_SECRET as string,
-    { expiresIn: '1m' }
+    { expiresIn: '7d' }
   );
 };
 
