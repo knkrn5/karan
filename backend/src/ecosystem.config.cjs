54a269eb2cfe294bc87631da0971d73473cc4666
Karan <xkaranx5@gmail.com>
2025-03-15 11:06:16 +0530

    pm2 added

---
+++ backend/src/ecosystem.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [{
+      name: "karan",
+      script: "./dist/server.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "200M"
+    }]
+  };
+  
