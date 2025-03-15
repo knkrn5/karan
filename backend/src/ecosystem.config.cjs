43ed5fb13dcdd46fd47e19070528402bd028f990
Karan <xkaranx5@gmail.com>
2025-03-15 11:27:25 +0530

    PRODUCTION

--- backend/src/ecosystem.config.cjs
+++ backend/src/ecosystem.config.cjs
@@ -1,11 +1,21 @@
 module.exports = {
-    apps: [{
-      name: "karan",
-      script: "./dist/server.js",
+  apps: [
+    {
+      name: process.env.NODE_ENV === "PRODUCTION" ? "karan" : "karan-dev",
+      script: process.env.NODE_ENV === "PRODUCTION" ? "./dist/server.js" : "src/server.ts",
+      interpreter: process.env.NODE_ENV === "PRODUCTION" ? "node" : "ts-node",
       instances: 1,
       autorestart: true,
-      watch: false,
-      max_memory_restart: "200M"
-    }]
-  };
-  
+      watch: process.env.NODE_ENV !== "PRODUCTION", // Watch files only in dev
+      max_memory_restart: "200M",
+      env: {
+        NODE_ENV: "development",
+        PORT: 3000
+      },
+      env_production: {
+        NODE_ENV: "PRODUCTION",
+        PORT: 5000
+      }
+    }
+  ]
+};
