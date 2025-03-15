1b6008573cf4a5c4926a7cd7ffb3ce7a480329c6
Karan <xkaranx5@gmail.com>
2025-03-15 13:06:15 +0530

    runtime added

--- backend/src/ecosystem.config.cjs
+++ backend/src/ecosystem.config.cjs
@@ -2,7 +2,8 @@ module.exports = {
   apps: [{
     name: "karan",
     script: "./dist/server.js",
-    instances: 1,
+    instances: 2,
+    exec_mode: 'cluster',
     autorestart: true,
     watch: false,
     max_memory_restart: "200M"
