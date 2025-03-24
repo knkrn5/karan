3320b52a052b6416b16c5d2d8b549aa98420e0d5
Karan <xkaranx5@gmail.com>
2025-03-24 09:02:02 +0530

    fining

--- frontend/src/pages/home/heroSectionOne.tsx
+++ frontend/src/pages/home/heroSectionOne.tsx
@@ -1,17 +1,12 @@
 export default function heroSection() {
   return (
-    <div className="flex items-center h-[200px] justify-center w-full bg-conic from-white via-gray-600 to-white to-100% dark:from-slate-600 dark:via-slate-900  dark:to-gray-600  dark:to-100%">
-      <div className="text-center text-6xl font-bold  italic leading-none">
+    <div className="flex items-center h-[200px] justify-center w-full bg-conic from-white via-neutral-600 to-white to-100% dark:from-slate-600 dark:via-slate-900  dark:to-gray-600  dark:to-100%">
+      <div className="text-center text-6xl font-bold  italic leading-none ">
         <span className="relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-gray-700 dark:before:bg-slate-900">
           <span className="relative text-gray-200 dark:text-gray-300">Protfolio</span>
         </span>
-        <div className="relative w-[5px] mx-auto before:content-[''] before:absolute before:w-[5px] before:h-25 before:top-1 before:left-1/2 before:-translate-x-1/2 before:bg-gray-500 dark:before:bg-gray-400 dark:before:shadow-sky-950"></div>
-
-
+        <div className="relative w-[5px] mx-auto before:content-[''] before:absolute before:w-[5px] before:h-17 before:top-1 before:left-1/2 before:-translate-x-1/2 before:bg-gray-400 dark:before:bg-gray-400 dark:before:shadow-sky-950"></div>
       </div>
     </div>
   );
 }
-
-{/* <div className="w-[5px] h-5 mx-auto bg-gray-200 dark:bg-gray-400"></div> */}
-
--- frontend/src/pages/home/heroSectionTwo.tsx
+++ frontend/src/pages/home/heroSectionTwo.tsx
@@ -1,7 +1,7 @@
 export default function heroSectionTwo() {
     return (
       <div className="flex items-center h-[200px] justify-center w-full bg-radial-[at_55%_75%] from-neutral-800 to-gray-200 dark:from-gray-300 dark:to-slate-900 to-65%">
-        <blockquote className="text-center text-6xl font-bold text-gray-100 dark:text-gray-300 italic leading-none">
+        <blockquote className="text-center text-6xl font-bold text-gray-100 dark:text-gray-300 italic leading-none ">
           Life is short <br></br>
           <span className="relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-gray-200 dark:before:bg-slate-500">
             <span className="relative text-black dark:text-gray-950">Act Now...</span>
