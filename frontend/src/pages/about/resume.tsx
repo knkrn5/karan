0d81aa0199022915218e6b48ac293fa10e74a46e
Karan <xkaranx5@gmail.com>
2025-03-30 19:11:08 +0530

    refactor resume page: adjust styling with margin and padding for better layout

--- frontend/src/pages/about/resume.tsx
+++ frontend/src/pages/about/resume.tsx
@@ -6,7 +6,7 @@ export default function Resume() {
   }, []);
 
   return (
-    <div className="h-screen bg-gray-200 text-black dark:bg-slate-800 dark:text-white">
+    <div className="h-screen m-2 p-2 bg-gray-200 text-black dark:bg-slate-800 dark:text-white">
       Redirecting to Resume
     </div>
   );
