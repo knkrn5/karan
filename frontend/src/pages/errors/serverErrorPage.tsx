8e9c69b123c37d01e12ee6bf1078fe1c5b0e8f55
Karan <xkaranx5@gmail.com>
2025-04-11 12:58:52 +0530

    fix(serverErrorPage): update error message and button text for clarity

--- frontend/src/pages/errors/serverErrorPage.tsx
+++ frontend/src/pages/errors/serverErrorPage.tsx
@@ -15,7 +15,7 @@ const ServerErrorPage = ({ errorMsg }: ServerErrorPageProps) => {
 
         {/* Error Message */}
         <h2 className="mt-4 text-2xl font-semibold text-gray-800 dark:text-gray-200">
-          Oops! No Posts Found.
+          Oops! Internal Server Error.
         </h2>
         {/* <p className="mt-2 text-lg text-red-500">Failed to Load the Blog Posts. Please try again later.</p> */}
         <p className="mt-2 text-lg text-red-500">{errorMsg}.</p>
@@ -33,9 +33,8 @@ const ServerErrorPage = ({ errorMsg }: ServerErrorPageProps) => {
         <Link
           to="/"
           className="mt-6 inline-block px-6 py-3 text-lg font-medium text-white bg-blue-600 rounded-lg shadow-md transition duration-300 hover:bg-blue-700 hover:shadow-lg dark:bg-blue-500 dark:hover:bg-blue-600"
-          onClick={() => navigate(-1)}
         >
-          Go back
+          Go Home
         </Link>
       </div>
     </div>
