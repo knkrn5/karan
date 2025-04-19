c1bbd1c9f3b7f60b7069c5b86694d4c3760e4b30
Karan <xkaranx5@gmail.com>
2025-04-19 11:36:19 +0530

    trust proxy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -24,7 +24,7 @@ app.use(cors(corsOptions));
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ limit: '16kb', extended: true }));
 app.use(cookieParser());
-// app.set('trust proxy', 'loopback'); // Trust only localhost/loopback IPs
+app.set('trust proxy', true); // Trust only the leftmost IP address
 
 // Health Check
 app.get('/health', (req, res) => {
