f2665c393a3319e9e46f910079cd51592aa922c5
Karan <xkaranx5@gmail.com>
2025-04-21 17:04:58 +0530

    skeleton loading

--- frontend/src/pages/auth/authButtons.tsx
+++ frontend/src/pages/auth/authButtons.tsx
@@ -5,7 +5,7 @@ import UserAccount from './userAccount.js';
 import { useAuthStore } from '../../stores/auth/authStore.js';
 import { useProfileStore } from '../../stores/profile/profileStore.js';
 import { useAuthCheck } from '../../hooks/authCheckHook.js';
-import { AuthButtonsSkeletonLoading } from '../../components/skeletonLoadings.js';
+import { AuthButtonsSkeletonLoading } from '../../components/ui/skeletonLoadings.js';
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
