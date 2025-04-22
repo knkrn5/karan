cd715cf1f4765413127082e138d2cd5064828a5a
Karan <xkaranx5@gmail.com>
2025-04-22 08:28:03 +0530

    svg rm

--- frontend/src/pages/auth/register.tsx
+++ frontend/src/pages/auth/register.tsx
@@ -3,10 +3,10 @@ import { Link, useNavigate } from 'react-router';
 import axios from 'axios';
 import { FaRegEye, FaRegEyeSlash, FaRegSave, FaRegCheckCircle } from 'react-icons/fa';
 import { FaRepeat } from 'react-icons/fa6';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { CiEdit } from 'react-icons/ci';
 import { ICnotificationMsg } from '../../components/notifications/ICnotificationMsg.js';
 import { useAuthStore } from '../../stores/auth/authStore.js';
-import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { verifyExistingUser, sendEmailOtp, verifyEmailOtp } from '../../utils/auth.utils.js';
 import { useTRpopupNotificationStore } from '../../stores/popup/TRpopupNotificationStore.js';
 import { useICnotificationMsgStore } from '../../components/stores/ICnotificationMsgStore.js';
@@ -593,26 +593,7 @@ export default function Register() {
         >
           {isSigningUp ? (
             <>
-              <svg
-                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
+              <AiOutlineLoading3Quarters className="animate-spin -ml-1 mr-3 h-5 w-5 text-white font-bold" />
               {!registrationVerification.isOptSent
                 ? 'Sending OTP...'
                 : registrationVerification.isOptSent && !registrationVerification.isOptVerified
