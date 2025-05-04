3216d9de6f558e8b36939ba277d0c06a9bda70a4
Karan <xkaranx5@gmail.com>
2025-05-04 20:22:48 +0530

    formated

--- frontend/src/pages/contact/getContactMsgFromDb.tsx
+++ frontend/src/pages/contact/getContactMsgFromDb.tsx
@@ -54,9 +54,9 @@ export default function GetContactMsgFromDb() {
 
   return (
     <div className="p-4 flex flex-col items-center bg-gradient-to-br from-gray-300 via-gray-100 to-gray-300 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 rounded-lg hover:shadow-lg transition-hover duration-300">
-        <h4 className="relative font-extrabold text-lg mb-2 text-black dark:text-white ">
-          {profileEmail}
-        </h4>
+      <h4 className="relative font-extrabold text-lg mb-2 text-black dark:text-white ">
+        {profileEmail}
+      </h4>
       <div className="max-h-80 overflow-auto">
         {isGettingUserMsgs ? (
           <SeeContactMsgFromDbSkeletonLoading />
