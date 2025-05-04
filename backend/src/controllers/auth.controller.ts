00e5589425d1e14d3fed89fabc27a8e10e8c5c15
Karan <xkaranx5@gmail.com>
2025-05-04 22:17:25 +0530

    path added

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -62,11 +62,14 @@ export class AuthController {
           secure: true,
           sameSite: 'none',
           maxAge: 10 * 60 * 1000,
+          path: '/'
         })
         .cookie('refreshToken', response.data.refreshToken, {
           httpOnly: true,
           secure: true,
           sameSite: 'none',
+          maxAge: 7 * 24 * 60 * 60 * 1000,
+          path: '/'  
         })
         .json(response);
     } catch (error: any) {
