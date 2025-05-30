e0d10d193e0218b6b1cd96e994d98455150c7b9b
Karan <xkaranx5@gmail.com>
2025-05-30 10:55:43 +0530

    skeleton loading fixing

--- frontend/src/pages/affiliates/affiliateProductsPage.tsx
+++ frontend/src/pages/affiliates/affiliateProductsPage.tsx
@@ -43,70 +43,73 @@ const AffiliateProductsPage = () => {
         <AffiliateSearchAndCart />
       </div>
 
-      {products.length === 0 && !isFetchingProducts ? (
-        <p className="text-center text-gray-500">No products available.</p>
-      ) : (
-        <div className="grid max-xs:grid-cols-1 max-lg:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 max-w-7xl mx-auto">
-          {isFetchingProducts &&
-            Array.from({ length: 6 }).map((_, index) => (
-              <AffiliateProductCardSkeletonLoading key={index} />
-            ))}
+      <div className="grid max-xs:grid-cols-1 max-lg:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 max-w-7xl mx-auto">
+        {products.length === 0 && isFetchingProducts ? (
+          Array.from({ length: 6 }).map((_, index) => (
+            <AffiliateProductCardSkeletonLoading key={index} />
+          ))
+        ) : (
+          <>
+            {products.map(product => (
+              <div
+                key={product.id}
+                className="flex flex-col @container/card bg-white dark:bg-dark rounded-2xl overflow-hidden shadow-md hover:shadow-xl transition-shadow duration-300"
+              >
+                <img src={product.image} alt={product.name} className="h-48 w-full object-cover" />
 
-          {products.map(product => (
-            <div
-              key={product.id}
-              className="flex flex-col @container/card bg-white dark:bg-dark rounded-2xl overflow-hidden shadow-md hover:shadow-xl transition-shadow duration-300"
-            >
-              <img src={product.image} alt={product.name} className="h-48 w-full object-cover" />
-
-              <div className="p-4 flex flex-col flex-grow space-y-1">
-                <h2 className="text-xl font-bold text-gray-800 dark:text-gray-200">
-                  {product.name}
-                </h2>
-                <div className="flex items-center gap-2">
-                  <span className="text-sm text-indigo-500">{product.category}</span>
-                  <span className="text-sm text-gray-600 dark:text-gray-300 bg-slate-100 dark:bg-slate-800 rounded-full px-2">
-                    {product.brand}
-                  </span>
-                </div>
-                <p
-                  className="text-sm text-gray-600 dark:text-gray-300 flex-grow cursor-pointer"
-                  onClick={() =>
-                    setExpandedDescriptionId(prev => (prev === product.id ? null : product.id))
-                  }
-                >
-                  {expandedDescriptionId === product.id
-                    ? product.description
-                    : `${product.description.slice(0, 50)}...`}
-                </p>
-
-                <p className="text-lg font-bold text-gray-900 dark:text-gray-200">
-                  ₹{product.price}
-                </p>
-
-                <div className="flex gap-2 mt-2  flex-col @card/sm:flex-row">
-                  <a
-                    href={product.affiliateLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="flex-1 inline-flex items-center justify-center gap-2 text-white bg-indigo-600 px-4 py-2 rounded-lg hover:bg-indigo-700 transition"
+                <div className="p-4 flex flex-col flex-grow space-y-1">
+                  <h2 className="text-xl font-bold text-gray-800 dark:text-gray-200">
+                    {product.name}
+                  </h2>
+                  <div className="flex items-center gap-2">
+                    <span className="text-sm text-indigo-500">{product.category}</span>
+                    <span className="text-sm text-gray-600 dark:text-gray-300 bg-slate-100 dark:bg-slate-800 rounded-full px-2">
+                      {product.brand}
+                    </span>
+                  </div>
+                  <p
+                    className="text-sm text-gray-600 dark:text-gray-300 flex-grow cursor-pointer"
+                    onClick={() =>
+                      setExpandedDescriptionId(prev => (prev === product.id ? null : product.id))
+                    }
                   >
-                    <FaShoppingBag size={16} />
-                    Buy Now
-                  </a>
-                  <button
-                    type="button"
-                    className="flex-1 inline-flex items-center justify-center gap-2 text-indigo-600 border border-indigo-600 px-4 py-2 rounded-lg hover:bg-indigo-50 transition cursor-pointer"
-                  >
-                    <FaShoppingCart size={16} />
-                    Add to Cart
-                  </button>
+                    {expandedDescriptionId === product.id
+                      ? product.description
+                      : `${product.description.slice(0, 50)}...`}
+                  </p>
+
+                  <p className="text-lg font-bold text-gray-900 dark:text-gray-200">
+                    ₹{product.price}
+                  </p>
+
+                  <div className="flex gap-2 mt-2  flex-col @card/sm:flex-row">
+                    <a
+                      href={product.affiliateLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="flex-1 inline-flex items-center justify-center gap-2 text-white bg-indigo-600 px-4 py-2 rounded-lg hover:bg-indigo-700 transition"
+                    >
+                      <FaShoppingBag size={16} />
+                      Buy Now
+                    </a>
+                    <button
+                      type="button"
+                      className="flex-1 inline-flex items-center justify-center gap-2 text-indigo-600 border border-indigo-600 px-4 py-2 rounded-lg hover:bg-indigo-50 transition cursor-pointer"
+                    >
+                      <FaShoppingCart size={16} />
+                      Add to Cart
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
-      )}
+            ))}
+          </>
+        )}
+
+        {products.length === 0 && !isFetchingProducts && (
+          <p className="text-center text-gray-500">No products available.</p>
+        )}
+      </div>
     </div>
   );
 };
