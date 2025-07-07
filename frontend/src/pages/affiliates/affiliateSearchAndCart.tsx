d32e5968b67be3bada59495e14a1a69e253a76b4
Karan <xkaranx5@gmail.com>
2025-07-07 15:16:30 +0530

    after filter map the object

--- frontend/src/pages/affiliates/affiliateSearchAndCart.tsx
+++ frontend/src/pages/affiliates/affiliateSearchAndCart.tsx
@@ -42,24 +42,27 @@ export default function AffiliateSearchAndCart({
     setIsCartOpen(false);
   }
 
-  // const matchingProductNames = products
-  //   .filter(product =>  product.name.toLowerCase().includes(searchedProduct.trim().toLowerCase()))
-  //   .map(product => product.name);
-
-  const matchingProductNames = products.filter(product => {
-    const isNameMatch = product.name.toLowerCase().includes(searchedProduct.trim().toLowerCase());
-    const isBrandMatch = product.brand.toLowerCase().includes(searchedProduct.trim().toLowerCase());
-    const isCategoryMatch = product.category
-      .toLowerCase()
-      .includes(searchedProduct.trim().toLowerCase());
-    const isSubCategoryMatch = product.subCategory?.some(subCat =>
-      subCat.toLowerCase().includes(searchedProduct.trim().toLowerCase())
-    );
-    const isTagMatch = product.tags?.some(tag =>
-      tag.toLowerCase().includes(searchedProduct.trim().toLowerCase())
-    );
-    return isNameMatch || isBrandMatch || isCategoryMatch || isSubCategoryMatch || isTagMatch;
-  });
+  const matchingProductNames = products
+    .filter(product => {
+      const isNameMatch = product.name.toLowerCase().includes(searchedProduct.trim().toLowerCase());
+      const isBrandMatch = product.brand
+        .toLowerCase()
+        .includes(searchedProduct.trim().toLowerCase());
+      const isCategoryMatch = product.category
+        .toLowerCase()
+        .includes(searchedProduct.trim().toLowerCase());
+      const isSubCategoryMatch = product.subCategory?.some(subCat =>
+        subCat.toLowerCase().includes(searchedProduct.trim().toLowerCase())
+      );
+      const isTagMatch = product.tags?.some(tag =>
+        tag.toLowerCase().includes(searchedProduct.trim().toLowerCase())
+      );
+      return isNameMatch || isBrandMatch || isCategoryMatch || isSubCategoryMatch || isTagMatch;
+    })
+    .map(product => ({
+      name: product.name,
+      category: product.category,
+    }));
 
   useEffect(() => {
     setSearchParams(prev => {
