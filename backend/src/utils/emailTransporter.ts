fd91f394b3c8204cf3e161d99a415f9ac33586c1
Karan <xkaranx5@gmail.com>
2025-07-08 10:50:32 +0530

    node mailer config changed

--- backend/src/utils/emailTransporter.ts
+++ backend/src/utils/emailTransporter.ts
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer';
 
 type EmailPropsTypes = {
-  toEmail: string;
+  toEmail: string | string[];
   subject: string;
   fallbackEmail: string;
   template: () => string;
@@ -12,16 +12,16 @@ export const emailTransporter = async ({
   subject,
   fallbackEmail,
   template,
-}: EmailPropsTypes) => {
+}: EmailPropsTypes): Promise<void> => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: 465,
-    secure: true, // true for 465, false for 587
+    port: Number(process.env.EMAIL_PORT),
+    secure: false, // true for port 465 (SSL), false for port 587 (STARTTLS)
     auth: {
       user: process.env.EMAIL_FROM,
       pass: process.env.EMAIL_PASS,
     },
-  });
+  } as nodemailer.TransportOptions);
 
   const mailOptions = {
     from: `"karan.email" <${process.env.EMAIL_FROM}>`,
