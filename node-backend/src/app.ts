22cf9eb232f15413f93b1c57204b3f003cc85ff0
Karan <151421344+knkrn5@users.noreply.github.com>
2026-05-13 15:52:45 +0000

    py cors changed

--- node-backend/src/app.ts
+++ node-backend/src/app.ts
@@ -9,9 +9,7 @@ dotenv.config({ path: '.env' });
 
 // CORS Configuration
 const corsOptions = {
-  origin: process.env.ENV === 'PRODUCTION' ? ['https://karan.email', 'https://www.karan.email'] : [
-    'http://localhost:5173',
-  ],
+  origin: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
---
+++ py-backend/src/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,70 @@
+from .configs.env import load_dotenv
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from .db.postgresDb import (
+    connect_db_and_create_table,
+)
+import os
+from .routes.affiliateproducts_routes import router as affiliate_products_router
+from .routes.admin_routes.adminAffiliateproducts_routes import (
+    router as admin_affiliate_products_router,
+)
+
+is_production = os.getenv("ENV") == "PRODUCTION"
+
+origins: list[str] = (
+    [
+        "https://karan.email",
+        "https://api.karan.email",
+    ]
+    if is_production
+    else ["http://localhost:5173"]
+)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # --- startup ---
+    res = connect_db_and_create_table()
+    print(res)
+    yield
+    # --- shutdown ---
+
+
+app = FastAPI(
+    lifespan=lifespan,
+    docs_url=None if is_production else "/docs",
+    redoc_url=None if is_production else "/redoc",
+    openapi_url=None if is_production else "/openapi.json",
+)
+
+
+app.add_middleware(
+    CORSMiddleware,  # pyrefly: ignore
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def main() -> str:
+    return "Hey api2🎉 :]"
+
+
+@app.get("/health")
+async def health() -> str:
+    return "api2 health ✅ :]"
+
+
+# routes
+app.include_router(
+    affiliate_products_router, prefix="/affiliate-products", tags=["affiliate-products"]
+)
+app.include_router(
+    admin_affiliate_products_router,
+    prefix="/admin/affiliate-products",
+    tags=["admin-affiliate-products"],
+)
